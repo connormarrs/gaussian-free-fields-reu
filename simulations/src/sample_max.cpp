a922d23470be11835c38ad3382be01465176373b
Connor Marrs <connormarrs@Connors-MacBook-Pro.local>
2022-06-22 14:00:26 -0400

    Started reorganizing project.

--- simulations/src/sample_max.cpp
+++ simulations/src/sample_max.cpp
@@ -18,6 +18,9 @@
 using namespace std;
 
 const double s = .25; // parameter of the DFGF
+const int num_nvals = 500; // number of n values to test
+const int n_start = 20; // start of n vals;
+const int n_end = 10000; //end of n vals;
 
 /**
  * Computes the rth eigenvalue for the nth discrete laplacian
@@ -52,21 +55,51 @@ double dfgf(double s, int n, double k, vector<double> randvec){
 }
 
 /**
+ * 
+ * 
+ */
+double compute_max(int n, vector<double> randvec) {
+	// iterate over all angles 2pik/n
+	double max = -DBL_MAX; // set max to smallest possible values computer knows
+	for(int k=0; k<n; k++){
+		// if we find larger value replace it
+		if(dfgf(s,n,1.0*k,randvec) > max) {
+			max = dfgf(s,n,1.0*k,randvec);
+		}
+	}
+	return max;
+}
+
+/**
  * Computes an n-dimensional Gaussian vector. More specifically, we compute
  * a vector of n i.i.d. N(0,1) random variables.
  */
 vector<double> gen_rand_vec(int size){
-	// TODO: figure out how to seed this so things are uncorrelated
+	// source: should guarantee that using std::normal_distribution
+	// generates independent samples
+	// https://stackoverflow.com/questions/21327249/how-to-generate
+	//-uncorrelated-random-sequences-using-c
+	// https://cplusplus.com/reference/random/normal_distribution/
 	default_random_engine generator;
-	normal_distribution<double> distribution(0.0,1.0);
 	
 	vector<double> randvec(size,0);
 	
 	for (int i=0; i<size; ++i) {
-		 randvec[i] = distribution(generator);
+		// for each n, use a new generator to guarantee independence
+		normal_distribution<double> distribution(0.0,1.0);
+		randvec[i] = distribution(generator);
 	}
 	
 	return randvec;
+}
+
+void test_rand_vec(){
+	int n = 12;
+	vector<double> random = gen_rand_vec(n);
+	cout << "Random Vector Sample" << '\n';
+	for(int k=0; k<n; k++) {
+		cout << random[k] << ',';
+	}
 }
 
 void test_arithmetic(){
@@ -74,7 +107,7 @@ void test_arithmetic(){
 	vector<double> V = { 0.0, 3.4, .69, .72, .85, .43, -.43, -1.2, -1.16, 0.8 };
 	//gen_rand_vec(num_vals);
 
-	cout << "Random Vector Sample" << '\n';
+	cout << "Vector Sample" << '\n';
 
 	for(int k=0; k<n; k++) {
 		cout << V[k] << ',';
@@ -118,13 +151,61 @@ void test_arithmetic(){
 }
 
 /**
+ * Method borrowed from here:
+ * https://stackoverflow.com/questions/27028226/python-linspace-in-c
+ */
+vector<int> linspace(int start_in, int end_in, int num_in) {
+	vector<int> linspaced;
+	
+	int start = static_cast<int>(start_in);
+	int end = static_cast<int>(end_in);
+	int num = static_cast<int>(num_in);
+	
+	if (num == 0) { 
+		return linspaced; 
+	}
+	if (num == 1) {
+		linspaced.push_back(start);
+		return linspaced;
+	}
+	
+	int delta = (end - start) / (num - 1);
+	
+	for(int i=0; i < num-1; ++i) {
+		linspaced.push_back(start + delta * i);
+	}
+	
+	return linspaced;
+}
+
+void test_max() {
+
+}
+
+void test_linspace() {
+
+}
+
+/**
  * The main file that runs the code
  */
 int main() {
-	const int num_vals = 10; // number of n values to test
+	const int num_nvals = 500; // number of n values to test
+	const int n_start = 20; // start of n vals;
+	const int n_end = 10000; //end of n vals;
 	const int num_trials = 3000; // number of trials to run for each n
 
+	/**
+	 *	TESTING SECTION
+	 */
+	test_rand_vec();
 	test_arithmetic();
+	test_max();
+
+	/**
+	 *	EXPERIMENT SECTION
+	 */
+	vector<int> nvals = linspace()// set the nvals
 
 	return 0;
 
