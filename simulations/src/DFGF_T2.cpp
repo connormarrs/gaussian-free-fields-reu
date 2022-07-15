f7e2a90d1d5b35c90ef8a0eb43d353f8d8f0c365
Tyler Campos <elevensor@ip-172-31-94-191.ec2.internal>
2022-07-15 17:35:56 +0000

    setting up DFGF_T2

--- simulations/src/DFGF_T2.cpp
+++ simulations/src/DFGF_T2.cpp
@@ -1,34 +1,73 @@
 #include "../headers/DFGF_T2.hpp"
-        void computeEigenVals();
-        double computeEigenVal(int k);
-        void computeEigenVectors();
-        void computeCoeffs();
-        void computeMaxVectors();
 
-        // constructors
-        DFGF_T2(double s, int n, int numberTrials, RandVec gaussianVector);
-		DFGF_T2()=default;
-        ~DFGF_T2()=default;
+void DFGF_T2::computeEigenVals(){
 
-        vector<vector<double>> getEigenVals();
-        /* returns f_p,q(theta_j, theta_k) */
-        double computeEigenFunctionPoint(int p, int q, int j, int k);
-        /* returns 2d vector containing f_p,q(j,k) at each point (j,k) in grid */
-        vector<vector<double>> computeEigenFunctionVector(int p, int q);
-        /* return the 4d array of all values f_p,q(j,k )*/
-        vector<vector<vector<vector<double>>>>  getEigenVectors();
+}
+double DFGF_T2::computeEigenVal(int p, int q){
 
-        /* returns f_p,q(theta_j, theta_k) */
-        double computeCoefficientPoint(int p, int q, int j, int k);
-        /* returns 2d vector containing f_p,q(j,k) at each point (j,k) in grid */
-        vector<vector<double>> computeCoefficientVector(int p, int q);
-        /* return the 4d array of all values f_p,q(j,k )*/
-        vector<vector<vector<vector<double>>>>  getCoefficients();
+}
+void DFGF_T2::computeEigenVectors(){
 
-        /* returns value of DFGF at point (j,k) in grid */
-        double evaluatePoint(int j, int k, vector<double> sampleVector);
-        /* returns 2d array containing values of DFGF at each point given the sample random vector*/
-        vector<vector<double>> evaluate(vector<double> sampleVector);
+}
+void DFGF_T2::computeCoeffs(){
 
-        void runTrials();
-        double computeEmpMean();
+}
+void DFGF_T2::computeMaxVectors(){
+
+}
+
+// constructors
+DFGF_T2::DFGF_T2(double s, int n, int numberTrials, RandVec gaussianVector){
+
+}
+DFGF_T2::DFGF_T2()=default{
+
+}
+DFGF_T2::~DFGF_T2()=default{
+
+}
+
+vector<vector<double>> DFGF_T2::getEigenVals(){
+
+}
+/* returns f_p,q(theta_j, theta_k) */
+double DFGF_T2::computeEigenFunctionPoint(int p, int q, int j, int k){
+
+}
+/* returns 2d vector containing f_p,q(j,k) at each point (j,k) in grid */
+vector<vector<double>> DFGF_T2::computeEigenFunctionVector(int p, int q){
+
+}
+/* return the 4d array of all values f_p,q(j,k )*/
+vector<vector<vector<vector<double>>>>  DFGF_T2::getEigenVectors(){
+
+}
+
+/* returns f_p,q(theta_j, theta_k) */
+double DFGF_T2::computeCoefficientPoint(int p, int q, int j, int k){
+
+}
+/* returns 2d vector containing f_p,q(j,k) at each point (j,k) in grid */
+vector<vector<double>> DFGF_T2::computeCoefficientVector(int p, int q){
+
+}
+/* return the 4d array of all values f_p,q(j,k )*/
+vector<vector<vector<vector<double>>>>  DFGF_T2::getCoefficients(){
+
+}
+
+/* returns value of DFGF at point (j,k) in grid */
+double DFGF_T2::evaluatePoint(int j, int k, vector<double> sampleVector){
+
+}
+/* returns 2d array containing values of DFGF at each point given the sample random vector*/
+vector<vector<double>> DFGF_T2::evaluate(vector<double> sampleVector){
+
+}
+
+void DFGF_T2::runTrials(){
+
+}
+double DFGF_T2::computeEmpMean(){
+
+}
