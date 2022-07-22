dff3808ae535b94c9ba1addbb1263ccad183115e
Tyler Campos <tyler.campos@yale.edu>
2022-07-22 03:30:15 +0000

    added core dump fix

--- simulations/src/driver.cpp
+++ simulations/src/driver.cpp
@@ -7,13 +7,13 @@
 #include <fstream>
 #include <string>
 #include <queue>
-#include <jsoncpp/json/json.h>
+// #include <jsoncpp/json/json.h>
 using namespace std;
 
 
 string fileName(int start_n, int end_n, string range) {
 	string name;
-	name += "d2_"+range+"_n"+to_string(start_n)+"-"+to_string(end_n)+".json";
+	name += "d2_"+range+"_n"+to_string(start_n)+"-"+to_string(end_n)+".csv";
 
 	return name;
 }
@@ -46,14 +46,14 @@ int main() {
 	// Runs through each n val to run the whole program
 	for(long unsigned int n_index=0; n_index < n_vals.size(); n_index++){
 		// Runs through each s val for a given n val
-		Json::Value jsonSmallRoot;
 		queue<future<double>> *tasks = new queue<future<double>>(); // make this pointer
 
-		/* set up filename and stuff for json */
-		string runName = "../../output/numTrials"+to_string(numTrials)+"_sRange_"+sRange+"_n_val"+to_string(n_vals[n_index])+".json";
-		jsonSmallRoot["info"]["nValue"] = n_vals[n_index];
-		jsonSmallRoot["info"]["numTrials"] = numTrials;
-		jsonSmallRoot["run"] = runName;
+		// Creates a name for the file
+		string runName = "../../output/" + to_string(n_vals[n_index]) + "_"+sRange+".csv";
+		std::ofstream myfile;
+		myfile.open (runName);
+
+
 		for (long unsigned int s_index=0; s_index<s_vals.size(); s_index++){
 			tasks->push(
 				async(launch::async, [](int nvalue, double svalue, int numTrials, RandVec randvec){
@@ -68,22 +68,15 @@ int main() {
 			);
 		}	
 		for(long unsigned int s_index=0; s_index<s_vals.size(); s_index++){
-			jsonSmallRoot["info"]["n"][to_string(n_vals[n_index])]["s"][to_string(s_vals[s_index])]["meanOfMaxima"] = tasks->front().get();
-			cout << "Thread n=" << n_vals[n_index] << " s=" << s_vals[s_index] << endl; // << " mean=" << tasks.front().get()
-			tasks->pop();
+			myfile << to_string(n_vals[n_index]) << ',' << to_string(s_vals[s_index]) << ',' << tasks.front().get() << endl;
+			tasks.pop();
+			cout << "Thread n=" << n_vals[n_index] << " s=" << s_vals[s_index] << endl;
 		}
 
-		/* clean up  queue once done */
-		tasks->~queue();
-		operator delete(tasks);
-
-		Json::StreamWriterBuilder builder;
-		builder["commentStyle"] = "None";
-		builder["indentation"] = "";
-		std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
-		std::ofstream outFile(runName);
-		writer -> write(jsonSmallRoot, &outFile);
-		outFile.close();
+		/*
+		* Writes the contents of cs (our json object) to a file
+		*/
+		myfile.close();
 	}
     	
 	cout << "Job done :)" << endl;
